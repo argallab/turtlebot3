bbc82d06c17caab4b11aa4878e3c52fe808aa8e3
MahdiehNejati <mahdiehnejatijavaremi2014@u.northwestern.edu>
2018-08-09 15:22:15 -0500

    tuned some motion shapes, still need more.

--- turtlebot3_dynamic_obstacle/src/turtlebot3_server.cpp
+++ turtlebot3_dynamic_obstacle/src/turtlebot3_server.cpp
@@ -9,136 +9,171 @@ Turtlebot3ActionServer::Turtlebot3ActionServer():
   //constructor
   ROS_INFO("in constructor of Turtlebot3ActionServer...");
 
-  initializeSubscribers();
   initializePublishers();
 
-    // do any other desired initializations here...specific to your implementation
+  // do any other desired initializations here...specific to your implementation
   as_.start(); //start the server running
   ROS_INFO("Turtlebot3 server sstarted");
 
 }
 
-void Turtlebot3ActionServer::initializeSubscribers()
-{
-  state_sub_ = nh_.subscribe<sensor_msgs::JointState>("joint_states", 1, &Turtlebot3ActionServer::getState, this);
-  odom_sub_  = nh_.subscribe<nav_msgs::Odometry>("odom", 1, &Turtlebot3ActionServer::getOdom, this);
-}
-
 void Turtlebot3ActionServer::initializePublishers()
 {
-  cmd_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+  cmd_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 5);
 }
 
-void Turtlebot3ActionServer::getOdom(const nav_msgs::Odometry::ConstPtr& odom)
+void Turtlebot3ActionServer::getOdom()
 {
-  position_ = odom->pose.pose.position;
+  try{
+    listener_.lookupTransform("/odom", "/base_footprint", ros::Time(0), transform_);
+  }
+  catch (tf::TransformException &ex) {
+    ROS_ERROR("%s", ex.what());
+    ros::Duration(1.0).sleep();
+  }
+  trans_ = transform_.getOrigin();
+  rot_ = transform_.getRotation();
+
+  position_.x = trans_.x();
+  position_.y = trans_.y();
+  position_.z = trans_.z();
+
+  tf::Matrix3x3 m(rot_);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+
+  rotation_.x = roll;
+  rotation_.y = pitch;
+  rotation_.z = yaw;
+  // ROS_INFO("x: %f, y: %f, z:%f, theta: %f", position_.x, position_.y, position_.z, rotation_.z);
 }
 
-void Turtlebot3ActionServer::getState(const sensor_msgs::JointState::ConstPtr& state)
+double Turtlebot3ActionServer::getRadian(double angle)
 {
-  float TICK2RAD = 0.001533981;
-  double last_pos, diff_pos, cur_pos, encoder; // TO DO:: CHECK ENCODER IF NEEDS TO BE INT
-  last_pos = 0.0;
-  diff_pos = 0.0;
-  encoder = 0.0;
-
-  // Origin is where turtlebot is at start of each request started
-  cur_pos = state->position[0];
-  diff_pos = cur_pos - last_pos;
-  encoder = encoder + (diff_pos/TICK2RAD);
-  right_encoder_ = encoder;
+  return angle*M_PI/180;
 }
 
-void Turtlebot3ActionServer::turn(float angle)
+double Turtlebot3ActionServer::wrapAngle(double angle)
 {
-  ros::Rate r(1);
-
-  if (init_state_)
+  angle = fmod(angle+M_PI, 2*M_PI);
+  if (angle<0)
   {
-    init_right_encoder_ = right_encoder_;
-    init_state_ = false;
+    angle += 2*M_PI;
   }
-
-  float diff_encoder = ((angle*3.14159/180) * 0.080) / (0.207 / 4096);
-  while (fabs(init_right_encoder_ - right_encoder_) < fabs(diff_encoder))
-  {
-
-    if (diff_encoder >= 0)
-    {
-      twist_.angular.z = -0.5;
-      ROS_INFO("SERVER: Turning -0.5");
-    }
-    else
-    {
-      twist_.angular.z = 0.5;
-      ROS_INFO("SERVER: Turning 0.5");
-    }
-    cmd_pub_.publish(twist_);
-    r.sleep();
-  }
-
-  init_state_ = true;
-  clearVelocities();
-
+  return angle - M_PI;
 }
 
-bool Turtlebot3ActionServer::getChangeInPosition(double length, int mode)
+void Turtlebot3ActionServer::move(double goal_x, double goal_y, double angle)
 {
-  bool desired_position;
-  if (mode == 0)
-  {
-    desired_position = fabs(position_.x - start_position_.x) < fabs(length);
-    ROS_INFO("SERVER: FIRST SIDE %f, length %f, bool %d",position_.x - start_position_.x, length, desired_position);
-  }
-  else if (mode == 1)
-  {
-    desired_position = fabs(position_.y - start_position_.y) < fabs(length);
-    ROS_INFO("SERVER: SECOND SIDE %f, length %f, bool %d",position_.x - start_position_.x, length, desired_position);
-  }
-  else if (mode == 2)
-  {
-    desired_position = fabs(position_.x - start_position_.x) > fabs(length);
-    ROS_INFO("SERVER: THIRD SIDE %f, length %f, bool %d",position_.x - start_position_.x, length, desired_position);
-  }
-  else if (mode == 3)
-  {
-    desired_position = fabs(position_.y - start_position_.y) > fabs(length);
-    ROS_INFO("SERVER: FOURTH SIDE %f, length %f, bool %d",position_.x - start_position_.x, length, desired_position);
-  }
-  return desired_position;
-}
+  ros::Rate r(10);
 
-void Turtlebot3ActionServer::goForward(double length, int count)
-{
-  ROS_INFO("SERVER: Trying to send %f", length);
-  double x_vel, z_vel;
-  ros::Rate r(1);
+  int last_rotation = 0;
+  int linear_speed = 1;
+  int angular_speed = 1;
 
-  if (length < 0)
-  {
-    x_vel = -0.1;
-  }
-  else
-  {
-    x_vel = 0.1;
-  }
+  double x_start, y_start, path_angle;
 
-  while(getChangeInPosition(length, count))
+  double goal_z = getRadian(angle);
+  getOdom();
+  double goal_distance = sqrt(pow(goal_x - position_.x, 2) + pow(goal_y - position_.y, 2));
+  double distance = goal_distance;
+
+  while (distance > 0.05)
   {
+    // check that preempt has not been requested by the client
     if (checkPreempt())
     {
       break;
     }
-    twist_.linear.x = x_vel;
-    ROS_INFO("x_vel %f", x_vel);
-    twist_.angular.z = 0.0;
+    getOdom();
+    x_start = position_.x;
+    y_start = position_.y;
+    path_angle = atan2(goal_y - y_start, goal_x - x_start);
+    path_angle = wrapAngle(path_angle);
+    ROS_INFO("x %f, y %f, angle %f", x_start, y_start, path_angle);
+
+    if (path_angle < -1*M_PI/4 || path_angle > M_PI/4)
+    {
+      if (goal_y < 0 && y_start < goal_y)
+      {
+        path_angle = -2 * M_PI + path_angle;
+      }
+      else if (goal_y >= 0 && y_start > goal_y)
+      {
+        path_angle = 2 * M_PI + path_angle;
+      }
+    }
+    if (last_rotation > M_PI - 0.1 && rotation_.z <= 0)
+    {
+      rotation_.z = 2 * M_PI + rotation_.z;
+    }
+    else if (last_rotation < -M_PI + 0.1 && rotation_.z >0)
+    {
+      rotation_.z = -2 * M_PI + rotation_.z;
+    }
+    distance = sqrt(pow(goal_x - x_start, 2) + pow(goal_y - y_start, 2));
+    twist_.linear.x = std::min(linear_speed*distance, 0.1);
+
+    twist_.angular.z = angular_speed * path_angle-rotation_.z;
+    if (twist_.angular.z > 0)
+    {
+      twist_.angular.z = std::min(twist_.angular.z, 1.5);
+    }
+    else
+    {
+      twist_.angular.z = std::max(twist_.angular.z, -1.5);
+    }
+    last_rotation = rotation_.z;
     cmd_pub_.publish(twist_);
-    feedback_.state.x = twist_.linear.x;
-    feedback_.state.z = twist_.angular.z;
-    as_.publishFeedback(feedback_);
-    ROS_INFO("SERVER: Executing, current state: %f, %f", feedback_.state.x, feedback_.state.z);
+    ROS_INFO("linear vel %f, angular vel %f", twist_.linear.x, twist_.angular.z);
     r.sleep();
   }
+
+  getOdom();
+
+  while( fabs(rotation_.z - goal_z) > 0.05)
+  {
+      // check that preempt has not been requested by the client
+      if (checkPreempt())
+      {
+        break;
+      }
+
+      getOdom();
+      if (goal_z >=0)
+      {
+        if (rotation_.z <= goal_z && rotation_.z >= goal_z - M_PI)
+        {
+          twist_.linear.z = 0.00;
+          twist_.angular.z = 0.5;
+        }
+        else
+        {
+          twist_.linear.z = 0.00;
+          twist_.angular.z = -0.5;
+        }
+      }
+      else
+      {
+        if (rotation_.z <= goal_z + M_PI && rotation_.z > goal_z )
+        {
+          twist_.linear.z = 0.00;
+          twist_.angular.z = -0.5;
+        }
+        else
+        {
+          twist_.linear.z = 0.00;
+          twist_.angular.z = 0.5;
+        }
+      }
+      cmd_pub_.publish(twist_);
+      // ROS_INFO("linear vel %f, angular vel %f", twist_.linear.x, twist_.angular.z);
+      // ROS_INFO("angular distance %f", fabs(rotation_.z - goal_z));
+      r.sleep();
+  }
+
+clearVelocities();
+
 }
 
 void Turtlebot3ActionServer::clearVelocities()
@@ -156,7 +191,6 @@ bool Turtlebot3ActionServer::checkPreempt()
     ROS_INFO("Preempted");
     as_.setPreempted(result_, "premepting due to request");
     clearVelocities();
-    // as_.setAborted(result_, "aborting");
     success_ = false;
     return 1;
   }
@@ -170,19 +204,12 @@ void Turtlebot3ActionServer::executeCB(const actionlib::SimpleActionServer<turtl
   double area = goal->goal.y;
   int count = goal->goal.z;
 
-  start_position_ = position_;
-  init_state_ = true;
-
-  // ROS_INFO("SERVER: In execution: Mode: %i, Area %f, Count %i", mode, area, count);
+  // start_position_ = position_;
+  // init_state_ = true;
 
   // helper variables
-  ros::Rate r(1);
+  ros::Rate r(10);
   success_ = true;
-
-  bool circle_mode = true;
-  bool half_patrol = false;
-  bool count_flag = false;
-  int circle_count = 0;
 
   // start executing the action
   for(int i=1; i<=count; i++)
@@ -193,64 +220,33 @@ void Turtlebot3ActionServer::executeCB(const actionlib::SimpleActionServer<turtl
       break;
     }
 
-    if (mode == 1) // straight traj
+    // move(0, 0, 180); //doesn't work
+    //   move(0, 0,-90); //dosen't work
+    //   move(0,0,30); // doens't work
+    if (mode==1)
     {
-      goForward(area, 0);
-      clearVelocities();
+      move(0.5, 0, 0);
+      r.sleep();
+      move(0, 0.5, 180);
+      // 2, 1, 30
     }
-
-    else if (mode == 2) // circular traj
-    {
-      ROS_INFO("SERVER: Circle Mode");
-      while(circle_mode)
-      {
-        if (checkPreempt())
-        {
-          circle_mode = false;
-          break;
-        }
-
-        if (position_.x < -area/2)
-        {
-          half_patrol = true;
-          count_flag = true;
-        }
-        else
-        {
-          twist_.linear.x = area/2;
-          twist_.angular.z = 0.5;
-        }
-        if (half_patrol && position_.x>0)
-        {
-          if(count_flag)
-          {
-            circle_count += 1;
-            count_flag = false;
-          }
-          half_patrol = false;
-          if (circle_count == count)
-          {
-            circle_mode = false;
-            clearVelocities();
-          }
-        }
-        cmd_pub_.publish(twist_);
-      }
+    else if (mode ==2)
+    {//togeter doesn't work 
+      move(0.5, 0, 0);
+      r.sleep();
+      move(0.5, 0.5, 90);
     }
-
-    else if (mode == 4) // square traj
+    else if (mode ==3)
     {
-      float sides[4] = {0};
-      sides[0] = area;
-      sides[1] = area;
-      for (int j=0; j<=3; j++)
-      {
-        ROS_INFO("Going forward for side length %f, side %i", sides[j], j);
-        goForward(sides[j], j);
-        r.sleep();
-        turn(-90);
-        r.sleep();
-      }
+      move(0, 1, 90); // works
+      r.sleep();
+      move(0.7, 0.7,0);
+    }
+    else if (mode ==4)
+    {// together doesn't work
+      move(2, 1,0); //works
+      r.sleep();
+      move(2, 1.2,90); //works
     }
   }
 
@@ -258,20 +254,16 @@ void Turtlebot3ActionServer::executeCB(const actionlib::SimpleActionServer<turtl
   {
     result_.result.x = position_.x;
     result_.result.y = position_.y;
-    result_.result.x = position_.z;
-    // ROS_INFO("Succeeded");
+    result_.result.theta = rotation_.z;
     // set the action state to succeeded
     as_.setSucceeded(result_);
   }
   else
   {
-    // ROS_INFO("Failed");
     // set the action state to failed
     as_.setAborted();
   }
 }
-
-
 
 int main(int argc, char** argv)
 {
